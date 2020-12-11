147bcaaa78aa45f8c3dad486f1f02c1b0bf4d941
MaryHooker <mc020791@yahoo.com>
2020-12-11 14:20:19 -0600

    printing with three double quotes

---
+++ ex9.py
@@ -0,0 +1,15 @@
+# Printing, Printing, Printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# using a back slach and 'n' puts the code starting on a new line
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+# using three double-quotes allows you to type muktiple sentences as opposed to just one or two quotations
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")

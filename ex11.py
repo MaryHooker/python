cc2352216ff653de62ff6225dd848439594d7780
MaryHooker <mc020791@yahoo.com>
2020-12-14 17:58:17 -0600

    using int to convert input from a string to an integer

--- ex11.py
+++ ex11.py
@@ -14,3 +14,7 @@ print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 age = input("How old are you?")
 print(age)
+
+# getting a number from someone using input and converting it from a string into an integer so I can use the input to do math
+x = int(input("Type a number: "))
+print(x + 2)
